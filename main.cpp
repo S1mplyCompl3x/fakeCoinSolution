3a6f9b70922f6bf8cb68d1c28e9c63dc8ce9ee57
s1mplycompl3x@outlook.com <imi@Owner.local>
2017-04-22 08:53:55 -0400

    finished main.cpp

--- main.cpp
+++ main.cpp
@@ -2,36 +2,28 @@
 //  main.cpp
 //  algorithmsFinal
 //
-//  Created by IMI  on 4/20/17.
+//  Created by IMI  on 4/18/17.
 //  Copyright © 2017 s1mplycompl3x. All rights reserved.
 //
 
-/* ---------------------------- TO DO ---------------------------------
- 
-    1. Implement search
-    2. Figure how the iteration through the loop should be
-    3. Implement a way to find how long each operation took
-*/
 
-/*  how to split vector into 2 
- std::vector<int> lines;
- // fill
- std::size_t const half_size = lines.size() / 2;
- std::vector<int> split_lo(lines.begin(), lines.begin() + half_size);
- std::vector<int> split_hi(lines.begin() + half_size, lines.end());
- */
 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <time.h>
 #include <stdlib.h>
+#include <chrono>
+#include <unistd.h>
+
 
 using namespace std;
 
 //Declarations
-void insertFakeCoin(vector<int> &v, int index);
-void findFakeCoin(vector<int> &v, int index);
+int insertFakeCoin(vector<int> &v, int index);
+void findFakeCoinDividenConquer(vector<int> &v, int index);
+void findFakeCoinBruteForce(vector<int> v, int index);
 
 
 
@@ -39,15 +31,30 @@ void findFakeCoin(vector<int> &v, int index);
 //Main Program
 
 int main(){
-    srand (time(NULL)); //get the time to be hella random
-    int num; //
-    for (num = 1000;num<=30000;num+=1000){
+    srand (time(NULL)); //get the time to be truly random
+    int num;
+    long long averageBruteForce=0;
+    long long averageDividenConquer=0;
+    for (num = 10000;num<=30000;num+=1000){
+        for (int i=1;i<=10;i++){
         vector<int> initialVector(num,0);
-        insertFakeCoin(initialVector, num);
-        for (vector<int>::iterator it = initialVector.begin() ; it != initialVector.end(); ++it){
-            cout <<*it<<" ";
+        int index = insertFakeCoin(initialVector, num);
+        vector<int> initialVectorCopy = initialVector;
+
+        auto start = std::chrono::high_resolution_clock::now();
+        findFakeCoinDividenConquer(initialVector, index);
+        auto elapsed = std::chrono::high_resolution_clock::now() - start;
+        averageDividenConquer += std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+
+
+        auto start1 = std::chrono::high_resolution_clock::now();
+        findFakeCoinBruteForce(initialVector, num);
+        auto elapsed1 = std::chrono::high_resolution_clock::now() - start1;
+        averageBruteForce += std::chrono::duration_cast<std::chrono::microseconds>(elapsed1).count();
         }
-        cout <<"\n-----------------------------------------------"<<endl;
+        cout<<"Divide and Conquer Average for "<<num<<" is "<<averageDividenConquer/10<<" microseconds"<<endl;
+        cout<<"BruteForce Average for "<<num<<" is "<<averageBruteForce/10<<" microseconds"<<endl;
+
     }
     
     return 0;
@@ -56,31 +63,68 @@ int main(){
 
 //Definition
 
-void insertFakeCoin(vector<int> &v, int index){
+int insertFakeCoin(vector<int> &v, int index){
+
     int r = rand() % index;
     v[r] = 1;
+    return r;
 }
 
-void findFakeCoin(vector<int> &v, int index){
+void findFakeCoinDividenConquer(vector<int> &v, int index){
+
 	size_t half = v.size() / 2;
-	vector<int> lo_temp(v.begin(), v.begin()+half);
-	vector<int> hi_temp(half + v.begin(), v.end());
-	bool found = false;
-	while (found == false){
-	//If found change found to true and change &v to that array
-	for (vector<int>::iterator it = lo_temp.begin() ; it != lo_temp.end(); ++it){
-			if (*it == 1){
-				found = true;
-				v = lo_temp;
-			}
-		}
-	for (vector<int>::iterator it = hi_temp.begin() ; it != hi_temp.end(); ++it){
-		if (*it == 1){
-				found = true;
-				v = hi_temp;
-			}
-		}
-	}	
-	
+    vector<int> lo_temp, hi_temp, temp1, temp2;
+    int remainder = v.size() % 2;
+    int rem = remainder;
+    int remPosition = (v.size() / 2);
+    /* Split the vector based on if remainder exists */
+    if (remainder == 0){
+	vector<int> temp1(v.begin(), v.begin()+half);
+        lo_temp = temp1;
+	vector<int> temp2(half + v.begin(), v.end());
+        hi_temp = temp2;
+    }
+    else if (remainder == 1){
+        vector<int> temp1(v.begin(), v.begin()+half);
+        lo_temp = temp1;
+        vector<int> temp2((remPosition+1) + v.begin(), v.end());
+        hi_temp = temp2;
+        remainder = v[remPosition];
+    }
+    /*---------Conditions---------*/
+
+    if (half == 1){
+        if (lo_temp[0] == 1){
+            return;}
+        else if (hi_temp[0] == 1){
+            return;}
+    }
+    if (remainder == 1){
+        return;
+    }
+    else if(index<half && remainder == 0){
+        v = lo_temp;
+        findFakeCoinDividenConquer(v, index);
+    }
+    else if(index>=half && remainder == 0){
+        v = hi_temp;
+        if (index > half){
+            index = (index) - (half +rem) ;
+            findFakeCoinDividenConquer(v, index);}
+        
+    }
+    
 }
+
+
+void findFakeCoinBruteForce(vector<int> v, int index){
+    for (int i=0;i<index;i++){
+        if (v[i] == 1){
+            i = index;
+        }
+    }
+    
+        
+}
+
 
